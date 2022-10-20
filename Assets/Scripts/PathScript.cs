9422320a0b3a784749f038b17783a43c28ba354b
Thomas Gelman <tcgelman@ncsu.edu>
2022-10-19 20:24:22 -0400

    pushing stuff and things

--- Assets/Scripts/PathScript.cs
+++ Assets/Scripts/PathScript.cs
@@ -4,33 +4,18 @@ using UnityEngine;
 
 public class PathScript : MonoBehaviour
 {
+    GameObject leftWall;
+    GameObject rightWall;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.name == "Player")
-        {
-            Debug.Log("Collided with Wall Spawn!");
-        }
-
-        if (collision.gameObject.name == "Wall Spawn")
-        {
-            Debug.Log("Collided with Wall Spawn!");
-        }
-
-        if (collision.gameObject.name == "Wall Despawn")
-        {
-            Debug.Log("Collided with Wall Despawn!");
-        }
+        leftWall = GetComponent<GameObject>();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        transform.position += new Vector3(0, 0.1f, 0);
+        
     }
 }
