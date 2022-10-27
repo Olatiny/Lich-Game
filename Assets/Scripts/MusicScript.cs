f6e7b77cd8f60dec5444e29aaef2df6b09172fd7
Camden <camdenmac@icloud.com>
2022-10-27 12:33:01 -0400

    Made music more better
    yep

--- Assets/Scripts/MusicScript.cs
+++ Assets/Scripts/MusicScript.cs
@@ -1,62 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MusicScript : MonoBehaviour
 {
-    public AudioSource ambience;
-
+    public AudioSource ambience; // has its clip stored inside
     public AudioSource music;
+    public AudioSource sfx;
 
     public AudioClip menuSong;
+    public AudioClip fallingSong;
 
-    public AudioClip song2;
+    public AudioClip deathCue; // all sfx will be played as oneshots through sfx
 
     // Start is called before the first frame update
     void Start()
     {
-        //Play();
+        ambience.volume = 0.02f; // ambience will fade in on the first call to menuMusic
+        ambience.Play(); // this makes it so the ambience can loop
+        
+        
+        // altered MenuMusic() for the first time
+        // Maybe add volume slider later
+        
+        // fade the ambience up VERY SLOW
+        StartCoroutine(FadeAudioSource.StartFade(ambience, 3.5f, 0.15f)); // does nothing if it is already high
 
         music.volume = 0.1f;
         music.clip = menuSong;
         music.Play();
-
-
-        ambience.volume = 0.1f;
-        ambience.Play();
-
-
     }
+
 
     private void Update()
     {
         
     }
 
-    void StartGame()
-    {
-        if (music.clip == menuSong)
-            music.clip = song2;
-        else
-            music.clip = menuSong;
-        music.Play();
-        //source.Play();
-
-        //source.clip = clip1;
-    }
 
     public void UpdateMusic(GameManager.gameState state) // this is the state to change to
     {
         switch (state)
         {
             case GameManager.gameState.menu:
-                // play the menu music
-                // if the ambience is not already playing, play that
+                // play the menu music, fade in the ambience
+                MenuMusic();
                 break;
 
             case GameManager.gameState.falling:
-                // play the falling music
-                // cut the ambience
+                // play the falling music, lower the ambience
+                FallingMusic();
                 break;
 
             case GameManager.gameState.loot_room:
@@ -65,9 +59,61 @@ public class MusicScript : MonoBehaviour
                 break;
 
             case GameManager.gameState.die:
-                // play the death cue once
-                // also play the ambience
+                // play the death cue once, fade up the ambience
+                DieMusic();
                 break;
         }
+    }
+
+    public void MenuMusic()
+    {
+        // fade the ambience up
+        StartCoroutine(FadeAudioSource.StartFade(ambience, 2f, 0.15f)); // does nothing if it is already high
+
+        music.clip = menuSong;
+        music.Play();
+    }
+
+    public void FallingMusic()
+    {
+        // lower the ambience slightly for the gameplay music
+        StartCoroutine(FadeAudioSource.StartFade(ambience, 2f, 0.07f));
+
+        music.clip = fallingSong;
+        music.Play();
+
+        //ambience.Pause();
+
+    }
+
+    public void DieMusic()
+    {
+        music.Stop(); // no music when die
+
+        // fade the ambience back up
+        StartCoroutine(FadeAudioSource.StartFade(ambience, 2f, 0.15f));
+        // then play the death cue
+        sfx.PlayOneShot(deathCue, 0.1f);
+    }
+}
+
+
+
+// code borrowed from online, fades audio out
+// use:
+// StartCoroutine(FadeAudioSource.StartFade(AudioSource audioSource, float duration, float targetVolume));
+public static class FadeAudioSource
+{
+    public static IEnumerator StartFade(AudioSource audioSource, float duration, float targetVolume)
+    {
+        float currentTime = 0;
+        float start = audioSource.volume;
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
+            yield return null;
+        }
+        yield break;
     }
 }
